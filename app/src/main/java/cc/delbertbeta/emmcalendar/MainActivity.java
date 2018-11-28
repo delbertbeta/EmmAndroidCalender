770cf16b242b4df7b295bc2148c0f84f8f8fcf9b
delbertbeta <delbertbeta@live.com>
2018-11-28 20:25:27 +0800

    Finished the style for girls.

--- app/src/main/java/cc/delbertbeta/emmcalendar/MainActivity.java
+++ app/src/main/java/cc/delbertbeta/emmcalendar/MainActivity.java
@@ -1,13 +1,38 @@
 package cc.delbertbeta.emmcalendar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int themeId = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (themeId == -1) {
+            themeId = R.style.AppTheme;
+        }
+        setTheme(themeId);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void homeClick(View v) {
+        if (themeId == R.style.AppTheme) {
+            themeId = R.style.AppThemeGirl;
+        } else {
+            themeId = R.style.AppTheme;
+        }
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+        setTheme(themeId);
+
+        setContentView(R.layout.activity_main);
+
     }
 }
